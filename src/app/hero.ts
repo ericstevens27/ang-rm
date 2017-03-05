9a08ffc10dcebdcd7b12be720757d641a0321744
ericstevens <eric@ericstevens.ca>
2017-03-05 10:35:00 -0500

    Added new elements to hero definition Added new data to first record in users.json

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -8,9 +8,43 @@ export class heroWeapons {
   itemBonus: number;
 }
 
+export class heroStatistics {
+  name: string;
+  description: string;
+  value: number;
+  bonusTotal: number;
+  bonusStat: number;
+  BonusRacial: number;
+}
+
+export class heroSkills {
+  name: string;
+  bonus: number;
+  stats: string;
+  rank: number;
+}
+
+export class items {
+  id: number;
+  name: string;
+  special: boolean;
+  quantity: number;
+  weight: number;
+  deadWeight: boolean;
+  notes: string;
+}
+
 export class Hero {
-    id: number;
-    name: string;
-    character: string;
-    weapons: heroWeapons[];
+  id: number;
+  name: string;
+  character: string;
+  race: string;
+  profession: string;
+  level: number;
+  hitPoints: number;
+  defenseBase: number;
+  stats: heroStatistics[];
+  skills: heroSkills[];
+  items: items[];
+  weapons: heroWeapons[];
 }
